1d37aec8eed6e9e0e1bca1e202ddefab5695dcb1
hossammoustafa404 <hossammoustafa404@gmail.com>
2023-10-31 18:16:09 +0200

    edit

--- server/src/config/nodemailer.ts
+++ server/src/config/nodemailer.ts
@@ -2,8 +2,8 @@ import * as nodermailer from "nodemailer";
 import config from "./config";
 
 const transporter = nodermailer.createTransport({
-  host: "live.smtp.mailtrap.io",
-  port: 2525,
+  host: "smtp.office365.com",
+  port: 587,
   auth: {
     user: config.nodemailer.user,
     pass: config.nodemailer.password,
