70d8d198f4fb813b016ecbca011be15a0a945002
hossammoustafa404 <hossammoustafa404@gmail.com>
2023-10-31 11:42:43 +0200

    edit

--- server/src/config/nodemailer.ts
+++ server/src/config/nodemailer.ts
@@ -2,7 +2,8 @@ import * as nodermailer from "nodemailer";
 import config from "./config";
 
 const transporter = nodermailer.createTransport({
-  service: "hotmail",
+  host: "smtp.office365.com",
+  port: 587,
   auth: {
     user: config.nodemailer.user,
     pass: config.nodemailer.password,
