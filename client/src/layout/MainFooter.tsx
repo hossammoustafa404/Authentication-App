d1971a6ca6003a4920db486125227dc4264d81a1
hossammoustafa404 <hossammoustafa404@gmail.com>
2023-11-01 05:16:16 +0200

    edit

--- client/src/layout/MainFooter.tsx
+++ client/src/layout/MainFooter.tsx
@@ -1,5 +1,20 @@
 const MainFooter = () => {
-  return "Footer";
+  return (
+    <footer className="mt-12">
+      <div className="container bg-black py-4">
+        <p className="text-center">
+          Developed by{" "}
+          <a
+            href="https://www.linkedin.com/in/hossam-moustafa-0492a3263/"
+            target="_blank"
+            className="text-orange-500"
+          >
+            Hossam Mousatafa
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default MainFooter;
